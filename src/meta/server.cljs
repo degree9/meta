51e4ad027ecc98327240b2e18a521d0833fc1d86
Matthew Ratzke <matt@degree9.io>
2018-10-14 22:48:25 -0600

    added debug to meta.server

--- src/meta/server.cljs
+++ src/meta/server.cljs
@@ -1,45 +1,66 @@
 (ns meta.server
-  (:require [feathers.app :as feathers]
+  (:require ["debug" :as dbg]
+            [feathers.app :as feathers]
             [meta.channels :as chan]))
+
+(def debug (dbg "degree9:meta:server"))
 
 (enable-console-print!)
 
-(def app (feathers/express (feathers/app)))
+(defn app []
+  (debug "Starting [meta] server")
+  (feathers/app))
 
 (defn with-defaults [app]
+  (debug "Loading server defaults")
   (-> app
+    feathers/express
     feathers/configuration
     feathers/json
     feathers/urlencoded
     feathers/static))
 
 (defn with-rest [app]
+  (debug "Loading server REST api")
   (-> app
     feathers/rest))
 
 (defn with-socketio [app]
+  (debug "Loading server SocketIO api")
   (-> app
     feathers/socketio))
 
 (defn with-authentication [app]
+  (debug "Loading server Authentication api")
   (-> app
     feathers/authentication))
 
 (defn with-channels [app]
+  (debug "Loading server Channels api")
   (-> app
     chan/join-anonymous))
 
 (defn using
-  ([path svc] (using app path svc))
-  ([app path svc] (.use app path svc)))
+  ([path svc]
+   (using app path svc))
+  ([app path svc]
+   (debug "Passing app.use call to feathers")
+   (.use app path svc)))
 
 (defn api
-  ([path svc hooks] (api app path svc hooks))
-  ([app path svc hooks] (feathers/api app path svc hooks)))
+  ([path svc hooks]
+   (api app path svc hooks))
+  ([app path svc hooks]
+   (debug "Passing app.api call to feathers")
+   (feathers/api app path svc hooks)))
 
 (defn listen
-  ([port] (listen app port))
-  ([app port] (.listen app port)))
+  ([port]
+   (listen app port))
+  ([app port]
+   (debug "Passing app.listen call to feathers")
+   (.listen app port)))
 
 (defn init! [fname]
+  (debug "Setting nodejs *main-cli-fn*")
   (set! *main-cli-fn* fname))
