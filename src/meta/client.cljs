19aa73ac25e7607a40ac9d8c03ce1945b36885ad
Matthew Ratzke <matt@degree9.io>
2019-12-08 00:46:33 -0700

    Update client.cljs

--- src/meta/client.cljs
+++ src/meta/client.cljs
@@ -2,54 +2,58 @@
   (:refer-clojure :exclude [find get update remove])
   (:require ["socket.io-client" :as io]
             ["jquery" :as jquery]
-            ["@feathersjs/client" :as client]
             [goog.object :as obj]
-            [feathers.client :as feathers]
-            [feathers.client.services :as svc]
-            [meta.promise :as prom]))
+            [feathers.client :as client]
+            [feathers.client.services :as svc]))
 
 ;; Feathers Client ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (def app client)
 
 (defn with-jquery [app uri]
-  (feathers/jquery app uri (jquery)))
+  (client/jquery app uri (jquery)))
 
 (defn with-socketio [app & [uri opts]]
-  (feathers/socketio app (io uri opts)))
+  (client/socketio app (io uri opts)))
 
 (defn with-authentication
   ([app]
    (with-authentication app (obj/get js/window "localStorage")))
   ([app storage]
-   (feathers/authentication app #js{:storage storage})))
+   (client/authentication app #js{:storage storage})))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Client Auth API ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (defn login! [app strategy & [opts]]
-  (feathers/authenticate app
+  (client/authenticate app
     (clj->js (merge opts {:strategy strategy}))))
 
 (defn logout! [app]
-  (feathers/logout app))
+  (client/logout app))
 
 (defn auth! [app]
-  (feathers/reauthenticate app))
+  (client/reauthenticate app))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Client Service API ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(def service feathers/service)
+(def service client/service)
 
-(def find    svc/find)
+(defn find [service & [params]]
+  (client/find service (clj->js params)))
 
-(def get     svc/get)
+(defn get [service id & [params]]
+  (client/get service id (clj->js params)))
 
-(def create  svc/create)
+(defn create [service data & [params]]
+  (client/create service (clj->js data) (clj->js params)))
 
-(def update  svc/update)
+(defn update [service id data & [params]]
+  (client/update service id (clj->js data) (clj->js params)))
 
-(def patch   svc/patch)
+(defn patch [service id data & [params]]
+  (client/patch service id (clj->js data) (clj->js params)))
 
-(def remove  svc/remove)
+(defn remove [service id & [params]]
+  (client/remove service id (clj->js params)))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 ;; Client Event API ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
